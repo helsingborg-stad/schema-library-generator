39397e927ef88faf545b367c6b91705181be9b55
Thor Brink <thor.brink@helsingborg.se>
2025-04-01 12:07:29 +0000

    fix: require global autoloader

--- generate.php
+++ generate.php
@@ -1,7 +1,9 @@
 #!/usr/bin/env php
 <?php
 
-if (file_exists(__DIR__.'/vendor/autoload.php')) {
+if(isset($GLOBALS['_composer_autoload_path'])) {
+    require $GLOBALS['_composer_autoload_path'];
+} elseif (file_exists(__DIR__.'/vendor/autoload.php')) {
     require __DIR__.'/vendor/autoload.php';
 } else {
     require __DIR__.'/../../autoload.php';
